f572f7d4f3426369189c70115291a36b58ed4a43
JoseCortez1 <jose.vazcortez@gmail.com>
2021-10-30 14:25:12 -0500

    [IMP] clean files how i won't use

---
+++ src/Components/Newbie/Order-summary-component/Card_order_sumary.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react"
+
+const Card_order_sumary = () => {
+	return <div>Card_order_sumary</div>
+}
+
+export default Card_order_sumary
---
+++ src/Components/Routes.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react"
+
+const Routes = () => {
+	return <div>Routes</div>
+}
+
+export default Routes
